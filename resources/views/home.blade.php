1378917ec2acc7435a54a7c2e9c8bb4bf1bda0b8
owenbcoding <eoghankb@gmail.com>
2025-08-11 05:39:01 +0100

    changed text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,14 @@
     <header class="py-5 bg-light border-bottom mb-4">
         <div class="container">
             <div class="text-center my-5">
-                <h1 class="fw-bolder">Welcome to Blog Home!</h1>
-                <p class="lead mb-0">A Bootstrap 5 starter layout for your next blog homepage</p>
+                <h1 class="fw-bolder">Blog app!</h1>
+                <p class="lead mb-0">Blog app homepage</p>
             </div>
         </div>
     </header>
     <div class="container">
         <div class="row">
-            <!-- Blog entries-->
+            <!-- Blog entries--> 
             <div class="col-lg-8">
                 <!-- Featured blog post-->
                 <div class="card mb-4">
