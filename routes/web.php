c4ff0bb2d3c08dfe03d664038a7b75531003d361
owenbcoding <eoghankb@gmail.com>
2025-08-27 15:14:59 +0100

    created route for article page

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ use App\Http\Controllers\HomeController;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::view('about', 'about')->name('about');
 Route::view('contact', 'contact')->name('contact');
+Route::view('article', 'article')->name('article');
