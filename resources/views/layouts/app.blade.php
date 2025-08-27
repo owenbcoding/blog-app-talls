7d92f01e36f032720dfe8089b7b50fc502cfe298
owenbcoding <eoghankb@gmail.com>
2025-08-27 16:43:24 +0100

    modified app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,21 +8,21 @@
     <meta name="author" content="" />
     <title>Blog Home</title>
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-   @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- <link href="{{ asset('css/styles.css') }}" rel="stylesheet" /> -->
+    <!-- @vite('resources/css/app.css') -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#!">Blog-app</a>
+            <a class="navbar-brand" href="{{ route('home') }}">Blog-app</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" aria-current="page" href="#">Blog</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('article') }}">Articles</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Blog</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">About</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('contact') }}">Contact</a></li>
                 </ul>
