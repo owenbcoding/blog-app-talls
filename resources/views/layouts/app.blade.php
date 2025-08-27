9514f9c89d2b1836b8c7bc2b74ee15ee7621e840
owenbcoding <eoghankb@gmail.com>
2025-08-27 14:43:06 +0100

    add main tag around @yield('content')

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,15 +23,17 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">About</a></li>
                     <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Blog</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">About</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('contact') }}">Contact</a></li>
                 </ul>
             </div>
         </div>
     </nav>
 
-    @yield('content')
+    <main>
+        @yield('content')
+    </main>
 
     <footer class="py-5 bg-dark">
         <div class="container">
