af032b2446636f5526398758a6ac44695277c659
owenbcoding <eoghankb@gmail.com>
2025-08-27 13:18:33 +0100

    removed home controller route and moved about to above contact

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
-Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::view('contact', 'contact')->name('contact');
