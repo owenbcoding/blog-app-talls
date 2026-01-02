5bbdc5490a40b418548a4d8c6f458ac0d75f527b
owenbcoding <eoghankb@gmail.com>
2026-01-02 16:08:39 +0000

    moved header section for image carousel into layout page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,13 @@
             </div>
         </div>
     </nav>
-
+    
+    <header class="py-5 bg-gray-100 border-b mb-4 h-60">
+        <div class="container mx-auto text-center mt-16">
+            <h1 class="text-3xl font-bold">A Personal Blog Site</h1>
+            <p class="text-lg text-gray-700 mb-0">Blog app carousel.</p>
+        </div>
+    </header>
     <main>
         @yield('content')
     </main>
