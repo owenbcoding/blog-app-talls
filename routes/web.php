5d2ab677430746f08d6cdfe7003233991440c67b
owenbcoding <eoghankb@gmail.com>
2026-01-02 21:55:18 +0000

    updated routes web to use postcontroller

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
 // Route::view('/', 'home')->name('home');
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show');
+
 Route::view('about', 'about')->name('about');
 Route::view('contact', 'contact')->name('contact');
 Route::view('article', 'article')->name('article');
