5bdb02f85d3ca59accbb8549bc19b1d598d27de2
owenbcoding <eoghankb@gmail.com>
2026-01-02 13:13:26 +0000

    modified home controller use eloquent instead of DB Table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,15 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\DB;
 
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $allCategories = DB::table('categories')->get();
-        return view('home', ['categories' => $allCategories]); 
+        $categories = Category::all();
+        $posts = Post::with('category')->orderBy('id', 'desc')->get();
+        
+        return view('home', [
+            'categories' => $categories,
+            'posts' => $posts
+        ]); 
     }
 }
