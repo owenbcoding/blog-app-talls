18efeeff57e0f9284a9cf0452d38fe23713625b1
owenbcoding <eoghankb@gmail.com>
2026-01-05 18:47:28 +0000

    created category button for navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <a href="{{ route('categories.index') }}" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500">
+                        {{ __('Categories') }}
+                    </a>
                 </div>
             </div>
 
