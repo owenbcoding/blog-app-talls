27b67ae78049d47d4e39deb217c0b47f175b9908
owenbcoding <eoghankb@gmail.com>
2026-01-15 10:13:37 +0000

    moved route to dashboard inside of middleware Route

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,16 @@ Route::get('/contact', function () {
 
 Route::get('/post/{post}', [\App\Http\Controllers\PostController::class, 'show'])->name('post.show');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     
     Route::resource('categories', CategoryController::class);
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
