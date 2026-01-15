a22d8c6a178c40df6b47debc1a05d654a4dc2bc1
owenbcoding <eoghankb@gmail.com>
2026-01-15 14:22:24 +0000

    created a config for blog post images

--- config/post_images.php
+++ config/post_images.php
@@ -1,9 +1,26 @@
 <?php
 
 return [
-    'api_base' => 'https://source.unsplash.com',
-    'fallback_query' => 'blog',
+    'api_type' => 'pexels', // 'pexels' or 'placeholder'
+    'pexels_api_key' => env('PEXELS_API_KEY', ''),
+    'coding_keywords' => [
+        'coding',
+        'programming',
+        'software development',
+        'web development',
+        'javascript',
+        'python',
+        'code',
+        'developer',
+        'computer programming',
+        'laptop code',
+        'coding workspace',
+        'programming language',
+        'software engineer',
+        'tech',
+        'computer science',
+    ],
     'overrides' => [
-        // post_id => 'https://source.unsplash.com/700x350/?laravel,framework',
+        // post_id => 'https://images.pexels.com/photos/1181677/pexels-photo-1181677.jpeg',
     ],
 ];
